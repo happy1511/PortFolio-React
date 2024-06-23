9a34ebd99441501907e5b14fcc3eabde1274e42f
happy1511 <140176910+happy1511@users.noreply.github.com>
2024-06-23 15:19:22 +0530

    scroll added to contact

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Tag = ({ label, link, icon }) => {
   return (
-    <button className="border border-borderTheme-dark dark:border-borderTheme-light rounded-full py-1 px-2 w-fit text-themebg-dark dark:text-themebg-light flex items-center gap-1 text-[15px] cursor-pointer opacity-80 hover:opacity-100 hover:shadow-inner">
+    <button
+      onClick={() => link && window.open(link)}
+      className="border border-borderTheme-dark dark:border-borderTheme-light rounded-full py-1 px-2 w-fit text-themebg-dark dark:text-themebg-light flex items-center gap-1 text-[15px] cursor-pointer opacity-80 hover:opacity-100 hover:shadow-inner"
+    >
       <span>{icon()}</span>
       <span>{label}</span>
     </button>

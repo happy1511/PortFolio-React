bc2622fd438af1cc2b48e1813b7178d74be43db6
happy1511 <hvp15112003@gmail.com>
2024-12-09 18:30:37 +0530

    animation change

--- src/components/CodeText.js
+++ src/components/CodeText.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CodeText = ({ children }) => {
   return (
-    <div className="w-full border border-borderTheme-dark dark:border-borderTheme-light rounded-md bg-[#000000d1] dark:bg-themebg shadow-lg">
+    <div className="w-full border border-borderTheme-dark dark:border-borderTheme-light rounded-md bg-[#000000d1] dark:bg-themebg shadow-lg ">
       <div className="flex gap-2 py-3 border-b px-3 border-borderTheme-light">
         <div className="p-2 rounded-full bg-primary1-light w-fit"></div>
         <div className="p-2 rounded-full bg-yellow-400 w-fit"></div>
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -4,10 +4,11 @@ const Tag = ({ label, link, icon }) => {
   return (
     <button
       onClick={() => link && window.open(link)}
-      className="border border-borderTheme-dark dark:border-borderTheme-light rounded-full py-1 px-2 w-fit text-themebg-dark dark:text-themebg-light flex items-center gap-1 text-[15px] cursor-pointer opacity-80 hover:opacity-100 hover:shadow-inner"
+      className="relative border border-borderTheme-dark dark:border-borderTheme-light rounded-full py-1 px-2 w-fit text-themebg-dark dark:text-themebg-light flex items-center gap-1 text-[15px] cursor-pointer opacity-80 hover:opacity-100 overflow-hidden group hover:shadow-md"
     >
-      <span>{icon()}</span>
-      <span>{label}</span>
+      <span className="absolute -inset-x-[4rem] -inset-y-[4rem] bg-gradient-to-tr from-primary1 to-primary2 transform -translate-x-full rotate-45 transition-transform duration-300 ease-in-out group-hover:translate-x-0"></span>
+      <span className="z-10">{icon()}</span>
+      <span className="z-10">{label}</span>
     </button>
   );
 };
